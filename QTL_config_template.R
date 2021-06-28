0ca7afe0445160a86604252103205a91d8dcc2ff
Brian Chen <bychen9@gmail.com>
2021-06-28 02:49:36 -0400

    Update pipeline

--- QTL_config_template.R
+++ QTL_config_template.R
@@ -1,24 +1,25 @@
 #ID for user's trait of interest. (Can be any string)
-trait = "T2D"
+trait = "TRAITNAME" 
 #path to the input files
-traitFilePath = "/project/voight_GWAS/wbone/CHD_T2D_2020_bivariate_scan/T2D_Vujkovic_et_al_dbSNP150_onlySNPs_data.txt"
+traitFilePath = "TRAITPATH" 
 #column IDs from trait file
-trait_A1col = "EA"
-trait_A2col = "NEA"
-trait_SNPcol = "name"
-trait_CHRcol = "CHR"
-trait_BPcol = "POS"
-trait_Pcol = "P"
-trait_Ncol = "N"
-trait_MAFcol = "EAF"
+trait_A1col = "A1COL" 
+trait_A2col = "A2COL" 
+trait_SNPcol = "SNPCOL" 
+trait_CHRcol = "CHRCOL" 
+trait_BPcol = "BPCOL" 
+trait_Pcol = "PCOL" 
+trait_Ncol = "NCOL" 
+trait_MAFcol = "MAFCOL" 
 #trait info not in the input file
 #traitType is set either to "cc" or "quant"
-traitType = "cc"
+traitType = "TRAITTYPE" 
 #This is the proportion of samples that are cases in a case control GWAS, if you are using a quantitative trait this should be set to "". traitProp = cases / case + controls
-traitProp = 0.162369 #look this up
+traitProp = TRAITPROP #look this up
 #locus information for running coloc. Currently this assumes these genomic positions to be from hg19
 chrom = CHROMOSOME
 colocStart = STARTBP
 colocStop = STOPBP
 lead_SNP = "SNPNUMBER" 
-
+#"eqtl" or "sqtl"
+qtlType = "QTL" 
