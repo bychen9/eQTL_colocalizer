591331e2146fc1ed58e2fa4035ba6182769b7b84
Brian Chen <bychen9@gmail.com>
2022-03-23 04:05:13 -0400

    updates to wrapper

--- setup_config.sh
+++ setup_config.sh
@@ -1,6 +1,8 @@
 #this bash config file allows you to set the paths to all the dependency file you need to run the qtl colocalizer pipeline
 #this config file is required for running the colocalizer pipeline on multiple loci at a time
 
+colocquial_dir="/project/voight_GWAS/bychen9/eQTL_colocalizer"
+
 #provide the path to plink refernce files to be used for plink commands
 plink_bfile="/project/voight_selscan/ksiewert/CardioMetaAnalysis/LDL_CHD_Bivar/LDClump/PlinkFilesOnlyRs/mergedBed"
 #provide the path to the plink ped files for the list of individuals you wish to use in your LD reference panel
